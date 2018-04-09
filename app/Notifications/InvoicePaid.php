9216594ab71e0dc53cef312df0b43a1ed4607fc8
quimgc <quimgonzalez@iesebre.com>
2018-04-09 21:58:35 +0200

    Initial version

---
+++ app/Notifications/InvoicePaid.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class InvoicePaid extends Notification
+{
+    use Queueable;
+
+    public $quantity;
+
+    public $nif;
+
+    /**
+     * InvoicePaid constructor.
+     * @param $quantity
+     * @param $nif
+     */
+    public function __construct($quantity, $nif)
+    {
+        $this->quantity = $quantity;
+        $this->nif = $nif;
+    }
+
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'quantity' => $this->quantity,
+            'nif' => $this->nif
+        ];
+    }
+}
